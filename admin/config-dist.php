0d2323322462afc5120308d1eb9f2b605d227a00
maxalonco13 <48546388+maxalonco13@users.noreply.github.com>
2021-11-20 15:39:11 +0200

    conf

---
+++ admin/config-dist.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', '<your_domain>/admin/');
+define('HTTP_CATALOG', '<your_domain>');
+
+// HTTPS
+define('HTTPS_SERVER', '<your_domain>/admin/');
+define('HTTPS_CATALOG', '<your_domain>');
+
+// DIR
+define('DIR_APPLICATION', '<your_path>/admin/');
+define('DIR_SYSTEM', '<your_path>/system/');
+define('DIR_LANGUAGE', '<your_path>/admin/language/');
+define('DIR_TEMPLATE', '<your_path>/admin/view/template/');
+define('DIR_CONFIG', '<your_path>/system/config/');
+define('DIR_IMAGE', '<your_path>/image/');
+define('DIR_CACHE', '<your_path>/system/storage/cache/');
+define('DIR_DOWNLOAD', '<your_path>/system/storage/download/');
+define('DIR_LOGS', '<your_path>/system/storage/logs/');
+define('DIR_MODIFICATION', '<your_path>/system/storage/modification/');
+define('DIR_UPLOAD', '<your_path>/system/storage/upload/');
+define('DIR_CATALOG', '<your_path>/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '***');
+define('DB_USERNAME', '***');
+define('DB_PASSWORD', '***');
+define('DB_DATABASE', 'octest');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config-dist.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', '<your_server>');
+
+// HTTPS
+define('HTTPS_SERVER', '<your_server>');
+
+// DIR
+define('DIR_APPLICATION', '<your_path>/catalog/');
+define('DIR_SYSTEM', '<your_path>/system/');
+define('DIR_LANGUAGE', '<your_path>/catalog/language/');
+define('DIR_TEMPLATE', '<your_path>/catalog/view/theme/');
+define('DIR_CONFIG', '<your_path>/system/config/');
+define('DIR_IMAGE', '<your_path>/image/');
+define('DIR_CACHE', '<your_path>/system/storage/cache/');
+define('DIR_DOWNLOAD', '<your_path>/system/storage/download/');
+define('DIR_LOGS', '<your_path>/system/storage/logs/');
+define('DIR_MODIFICATION', '<your_path>/system/storage/modification/');
+define('DIR_UPLOAD', '<your_path>/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '***');
+define('DB_USERNAME', '***');
+define('DB_PASSWORD', '***');
+define('DB_DATABASE', 'octest');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
